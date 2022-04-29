e62de6f7b4664d382b458385ef2a916ef871f098
Dylan Coxshall <63456774+DCoxshall@users.noreply.github.com>
2022-04-29 12:18:07 +0100

    Add files via upload

---
+++ monty.py
@@ -0,0 +1,68 @@
+import random
+
+#Performs one iteration of the Monty Hall problem.
+#"switch" represents whether or not the player has chosen to switch
+def monty(switch: bool) -> bool:
+    prizes = ["goat", "goat", "car"]
+    doors = {}
+
+    # Places prizes randomly behind doors
+    random.shuffle(prizes)
+    for i in range(3):
+        doors[i] = prizes[i]
+
+    # Player picks a random door to look behind
+    choice = random.choice([0, 1, 2])
+
+    # This is a dictionary containing the remaining choices
+    remaining_doors = {i: doors[i] for i in doors if i != choice}
+
+    # Ensure that Monty can only choose doors which contain a door
+    remaining_keys = list(remaining_doors.keys())
+    keys = [i for i in remaining_keys if remaining_doors[i] != "car"]
+    montys_choice = random.choice(keys)
+
+    #Perform the switch
+    if switch:
+        remaining_keys.remove(montys_choice)
+        choice = remaining_keys[0]
+
+    # Open the door and return whether or not the door has a car behind it.
+    return prizes[choice] == "car"
+
+if __name__ == "__main__":
+    n = int(input("Input iterations: "))
+    print("Performing simulation " + str(n) + " times for both switching and non-switching")
+
+    # Perform n simulations where the player chooses to switch
+    success = 0
+    failure = 0
+        
+    for i in range(n):
+        if (monty(True)):
+            success += 1
+        else:
+            failure += 1
+
+    success = round(success, 2)
+    failure = round(failure, 2)
+
+    print("Switch success rate = " + str(success/(success + failure) * 100) + "%")
+
+    # Perform n simulations where the player chooses to stay
+
+    success = 0
+    failure = 0
+        
+    for i in range(n):
+        if (monty(False)):
+            success += 1
+        else:
+            failure += 1
+
+    success = round(success, 2)
+    failure = round(failure, 2)
+
+    print("Stay success rate = " + str(success/(success + failure) * 100) + "%")
+
+
